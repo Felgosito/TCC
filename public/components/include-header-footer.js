d9f88c312a20bd2e415ff1cac0b4f1fa380c0786
felgosito <fg159802@gmail.com>
2026-03-10 20:07:57 -0300

    remoçao footer

--- public/components/include-header-footer.js
+++ public/components/include-header-footer.js
@@ -2,7 +2,3 @@
 fetch('components/header.html')
   .then(res => res.text())
   .then(html => { document.getElementById('header').innerHTML = html; });
-
-fetch('components/footer.html')
-  .then(res => res.text())
-  .then(html => { document.getElementById('footer').innerHTML = html; });
