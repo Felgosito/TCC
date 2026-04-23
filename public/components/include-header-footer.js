8c6183693f3dd315e1d1fb6d9bbd8518137136a5
felgosito <fg159802@gmail.com>
2026-04-23 20:07:43 -0300

    adição de footer

--- public/components/include-header-footer.js
+++ public/components/include-header-footer.js
@@ -2,3 +2,7 @@
 fetch('components/header.html')
   .then(res => res.text())
   .then(html => { document.getElementById('header').innerHTML = html; });
+
+  fetch('components/footer.html')
+  .then(res => res.text())
+  .then(html => { document.getElementById('footer').innerHTML = html; });
